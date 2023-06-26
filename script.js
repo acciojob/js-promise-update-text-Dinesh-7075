2244e0311d1e8cd9e93792033b897f94b8410bc0
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-06-26 07:22:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let x = new Promise(resolve => {
+	setTimeout(() => {
+	 resolve("Hello, world!");
+	}, 1000)
+});
+
+x.then((data) => {
+	let element = document.getElementById("output");
+	element.innerText = data;
+})
